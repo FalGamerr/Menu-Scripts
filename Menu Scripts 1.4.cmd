a3d05b1567c380de47fd9a61fc4cbc21d2912a7c
FalGamerr <135547175+FalGamerr@users.noreply.github.com>
2023-09-04 08:52:57 -0300

    Atualização do Menu Scripts 1.3 para Menu Scripts 1.4
    Correções:
    -Maiores correções de texto e de português.
    -Instalação de apps funciona em apenas uma linha de comando, tornando muito mais rápido e com menos erros.
    -Atualizado a lista de bloatwares.
    -Otimizações do script em geral.
    Adições:
    -Adicionado Microsoft Office na lista de instalação de pacotes de escritório, lembrando que é necessário ter uma licença em sua conta para utilizar ele.

--- Menu Scripts 1.4.cmd
+++ Menu Scripts 1.4.cmd
@@ -1,5 +1,6 @@
 @echo off
 title Menu Scripts
+Color 0D
 chcp 65001
 cls
 
@@ -35,7 +36,7 @@ goto menu
 cls
 echo ===============================================================================================================
 echo #                                                                                                             #
-echo #                                              Menu Scripts 1.3                                               #
+echo #                                              Menu Scripts 1.4                                               #
 echo #                                                                                                             #
 echo #                                            Selecione uma opção:                                             #
 echo #                                                                                                             #
@@ -104,6 +105,7 @@ winget uninstall WhatsApp.WhatsApp
 winget uninstall Microsoft.OneDrive
 winget uninstall Microsoft.Teams
 winget uninstall Microsoft.SkypeApp_kzf8qxf38zg5c
+winget uninstall Microsoft.OneDriveSync_8wekyb3d8bbwe
 winget uninstall MicrosoftTeams_8wekyb3d8bbwe
 winget uninstall Microsoft.Todos_8wekyb3d8bbwe
 winget uninstall Microsoft.Office.OneNote_8wekyb3d8bbwe
@@ -118,15 +120,7 @@ goto menu
 :programas
 cls
 :instalar
-winget install RARLab.WinRAR
-winget install Oracle.JavaRuntimeEnvironment
-winget install Google.Chrome
-winget install 9NZVDKPMR9RD --accept-package-agreements
-winget install XPDP273C0XHQH2 --accept-package-agreements
-winget install 9NCTDW2W1BH8 --accept-package-agreements
-winget install 9MVZQVXJBQ9V --accept-package-agreements
-winget install 9N95Q1ZZPMH4 --accept-package-agreements
-winget install 9N4D0MSMP0PT
+winget install RARLab.WinRAR Oracle.JavaRuntimeEnvironment Google.Chrome 9NZVDKPMR9RD XPDP273C0XHQH2 9NCTDW2W1BH8 9MVZQVXJBQ9V 9N95Q1ZZPMH4 9N4D0MSMP0PT 9N4D0MSMP0PT --accept-package-agreements
 if "%1"=="Y" exit/B
 pause
 goto menu
@@ -137,17 +131,25 @@ echo ===========================================================================
 echo #                                                                                                             #
 echo #                                          Selecione o App desejado:                                          #
 echo #                                                                                                             #
-echo #                                          1 - Libre Office                                                   #
-echo #                                          2 - Only Office                                                    #
-echo #                                          3 - WPS Office                                                     #
+echo #                                          1 - Microsoft Office                                               #
+echo #                                          2 - Libre Office                                                   #
+echo #                                          3 - Only Office                                                    #
+echo #                                          4 - WPS Office                                                     #
 echo #                                          0 - Voltar                                                         #
 echo #                                                                                                             #
 echo ===============================================================================================================
 set /p opcao=Opcao:
-if "%opcao%"=="1" goto libre
-if "%opcao%"=="2" goto only
-if "%opcao%"=="3" goto wps
+if "%opcao%"=="1" goto microsoft
+if "%opcao%"=="2" goto libre
+if "%opcao%"=="3" goto only
+if "%opcao%"=="4" goto wps
 if "%opcao%"=="0" goto menu
+goto menu
+
+:microsoft
+cls
+winget install 9WZDNCRD29V9
+pause
 goto menu
 
 :libre
@@ -243,13 +245,13 @@ echo # do sistema (bloatwares) instalando aplicativos básicos (Winrar, Java, Ch
 echo # codecs de vídeo e coloca no modo alto desempenho.                                                           #
 echo #   Opção 2 Limpar programas pré-instalados: Faz a mesma limpeza de bloatwares da Opção 1.                    #
 echo #   Opção 3 Instalar programas básicos: Instala os mesmos apps da Opção 1.                                    #
-echo #   Opção 4 Instalar Offices: Mostra opções de aplicativos para escritório gratuitos para instalação.         #
+echo #   Opção 4 Instalar Offices: Mostra opções de aplicativos para escritório para instalação.                   #
 echo #   Opção 5 Otimizar: Coloca no alto desempenho, atualiza todos os apps compatíveis com winget, executa       #
 echo # sfc /scannow, dism, desfragmenta todos os discos conectados e executa o chkdsk (Opcional).                  #
 echo #   Opção 6 Pesquisar App: Faz uma pesquisa com o termo digitado nos repositorios winget e pergunta qual      #
 echo # gostaria de instalar, copie a id do app selecionando com o mouse e apertando enter para copiar e cole a id  #
 echo # com Ctrl + V para instalar o app desejado.                                                                  #
-echo #   Opção 7 atualiza todos os apps compatíveis com winget                                                     #
+echo #   Opção 7 atualiza todos os apps compatíveis com winget.                                                    #
 echo #                                                                                                             #
 echo ===============================================================================================================
 pause
@@ -259,10 +261,10 @@ goto menu
 cls
 echo ===============================================================================================================
 echo #                                                                                                             #
-echo # Este script foi desenvolvido Por Fal_Gamerr, obrigado a RyanK_, Samuca e Ofernandofilo por auxiliar na      #
-echo # criação do script.                                                                                          #
-echo # Compatível com Windows 10 e 11 com a última atualização do instalador de aplicativos da Microsoft Store.    #
-echo # Caso queira contribuir com o projeto com uma doação, doe para a chave pix falgamerr@gmail.com               #
+echo #    Este script foi desenvolvido Por Fal_Gamerr, obrigado a RyanK_, Samuca e Ofernandofilo por auxiliar      #
+echo # na criação do script.                                                                                       #
+echo #    Compatível com Windows 10 e 11 com a última versão do instalador de aplicativos da Microsoft Store.      #
+echo #    Caso queira contribuir com o projeto com uma doação, doe para a chave pix: falgamerr@gmail.com           #
 echo #                                                                                                             #
 echo #                                               1 - Site oficial                                              #
 echo #                                               2 - Checar atualizações                                       #
