83b6aa13ecb826a354841232aff3207e6e411a0d
FalGamerr <135547175+FalGamerr@users.noreply.github.com>
2024-09-09 09:09:48 -0300

    Menu Scripts 1.8 SoftwareDATA Ed..cmd
    Adições:
    
    - Foi alterado a forma em que é realizada a remoção dos bloatwares, tornando o script absurdamente mais rápido.
    - Foi atualizado as opções de limpeza, feito alteração no código para que a opção 1 limpe apenas os preinstalados embarcados no windows 23H2 e 22H2 sequencialmente e a opção 2 limpar tudo de todas as versões anteriores um por um.
    - Alterado o Adobe Acrobat Reader para a versão Winget (Antes msstore).
    
    Correções:
    
    - Foi corrigido um problema com a desinstalação do Outlook.
    - Outras correções genéricas de código.

--- Menu Scripts 1.8 SoftwareDATA Ed..cmd
+++ Menu Scripts 1.8 SoftwareDATA Ed..cmd
@@ -4,6 +4,7 @@ chcp 65001
 cls
 
 :: BatchGotAdmin
+REM verifica o status de execução do script como administrador, solicita a permissão e inicia o script novamente
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
 	echo Requisitando privilégios de administrador...
@@ -32,9 +33,10 @@ pause
 goto menu
 
 :menu
+REM Menu principal do script com seleção de opção por número.
 cls
 echo ===============================================================================================================
-echo #                                    Menu Scripts 1.6 SoftwareDATA Edition                                    #
+echo #                                    Menu Scripts 1.8 SoftwareDATA Edition                                    #
 echo ===============================================================================================================
 echo #                                                                                                             #
 echo #                                            Selecione uma opção:                                             #
@@ -44,11 +46,12 @@ echo #                                         2 - Limpar programas pré-instala
 echo #                                         3 - Instalar programas básicos                                      #
 echo #                                         4 - Instalar Offices                                                #
 echo #                                         5 - Otimizar                                                        #
-echo #                                         6 - Pesquisar Apps                                                  #
-echo #                                         7 - Atualizar Apps                                                  #
-echo #                                         8 - Sobre                                                           #
+echo #                                         6 - Aplicativos                                                     #
+echo #                                         7 - Resolução de Problemas                                          #
+echo #                                         8 - Drivers                                                         #
+echo #                                         9 - Sobre                                                           #
 echo #                                         0 - Sair                                                            #
-echo #                                                                                  por Suporte SoftwareDATA   #
+echo #                                                                                                             #
 echo ===============================================================================================================
 set /p opcao=Opção:
 if "%opcao%"=="1" goto inicial
@@ -56,18 +59,26 @@ if "%opcao%"=="2" goto limpar
 if "%opcao%"=="3" goto programas
 if "%opcao%"=="4" goto office
 if "%opcao%"=="5" goto otimizar
-if "%opcao%"=="6" goto pesquisar
-if "%opcao%"=="7" goto atualizar
-if "%opcao%"=="8" goto sobre
+if "%opcao%"=="6" goto apps
+if "%opcao%"=="7" goto problemas
+if "%opcao%"=="8" goto drivers
+if "%opcao%"=="9" goto sobre
 if "%opcao%"=="0" exit
 goto menu
 
 :inicial
+REM Essa opção serve para pcs recém formatados, configura energia em alto desempenho, roda comandos condensados para desinstalar bloatwares e um trecho para instalar programas básicos e verifica a update dos aplicativos instalados.
 cls
 powercfg -setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 net user Suporte compaq9740 /add
 net localgroup Administradores Suporte /add
-call :desinstalar X
+:desinstalar
+REM Esse próximo código desinstala apenas bloatwares embarcados no Windows 11 23H2 todas as edições
+winget uninstall Clipchamp.Clipchamp_yxz26nhyzhsrt Microsoft.BingNews_8wekyb3d8bbwe Microsoft.GamingApp_8wekyb3d8bbwe Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe Microsoft.Todos_8wekyb3d8bbwe Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe Microsoft.OneDrive Microsoft.549981C3F5F10_8wekyb3d8bbwe 9WZDNCRDTBJJ 9WZDNCRDTBVB 9WZDNCRFJ3Q2 9WZDNCRFHVQM 9WZDNCRDTBVB --accept-source-agreements
+REM Esse próximo código desinstala apenas bloatwares embarcados no Windows 10 22H2 todas as edições
+winget uninstall 9NBLGGH4QGHW 9WZDNCRDTBJJ 9NBLGGH4R32N 9WZDNCRD29V9 9NBLGGH42THS 9NG1H8B3ZC7M Microsoft.SkypeApp_kzf8qxf38zg5c Microsoft.Office.OneNote_8wekyb3d8bbwe Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe Microsoft.XboxApp_8wekyb3d8bbwe --accept-source-agreements
+if "%1"=="X" exit /B
+winget uninstall Microsoft.OutlookForWindows_8wekyb3d8bbwe
 call :instalar Y
 winget upgrade --all --include-unknown
 pause
@@ -75,9 +86,8 @@ goto menu
 
 :limpar
 cls
-:desinstalar
-winget uninstall Clipchamp.Clipchamp_yxz26nhyzhsrt Microsoft.BingNews_8wekyb3d8bbwe Microsoft.GamingApp_8wekyb3d8bbwe Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe Microsoft.Todos_8wekyb3d8bbwe Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe Microsoft.OneDrive 9WZDNCRDTBJJ --accept-source-agreements
-winget uninstall 9NBLGGH4QGHW 9WZDNCRDTBJJ 9NBLGGH4R32N 9WZDNCRD29V9 9NBLGGH42THS 9NG1H8B3ZC7M Microsoft.SkypeApp_kzf8qxf38zg5c Microsoft.Office.OneNote_8wekyb3d8bbwe Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe Microsoft.XboxApp_8wekyb3d8bbwe --accept-source-agreements
+REM Os trechos seguintes desinstalam bloatwares individualmente desde a versão 1903 do Windows 10 até as mais recentes do windows 10 e 11, para que não hajam interrupções por erro no winget.
+call :desinstalar X
 winget uninstall 9NBLGGH4QGHW --accept-source-agreements
 winget uninstall 9WZDNCRDTBJJ
 winget uninstall 9NBLGGH4R32N
@@ -93,6 +103,7 @@ winget uninstall 9NH2GPH4JZS4
 winget uninstall 9NKSQGP7F2NH
 winget uninstall 9NWD3S85MTFB
 winget uninstall 9NBLGGH5L9XT
+winget uninstall 9WZDNCRDTBVB
 winget uninstall Clipchamp.Clipchamp_yxz26nhyzhsrt
 winget uninstall Disney.37853FC22B2CE_6rarf9sa4v8jt
 winget uninstall 22364Disney.ESPNBetaPWA_6rarf9sa4v8jt
@@ -122,19 +133,21 @@ winget uninstall Microsoft.XboxApp_8wekyb3d8bbwe
 winget uninstall Microsoft.GamingApp_8wekyb3d8bbwe
 winget uninstall Microsoft.OutlookForWindows_8wekyb3d8bbwe
 winget uninstall Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe
-if "%1"=="X" exit/B
+winget uninstall Microsoft.549981C3F5F10_8wekyb3d8bbwe
 pause
 goto menu
 
 :programas
 cls
+REM Próximo trecho instala o winrar, o java, chrome, firefox, acrobat reader e plugins de execução de vídeo e imagem
 :instalar
-winget install RARLab.WinRAR Oracle.JavaRuntimeEnvironment Google.Chrome 9NZVDKPMR9RD XPDP273C0XHQH2 9NCTDW2W1BH8 9MVZQVXJBQ9V 9N95Q1ZZPMH4 9N4D0MSMP0PT 9N4D0MSMP0PT --accept-package-agreements
-if "%1"=="Y" exit/B
+winget install RARLab.WinRAR Oracle.JavaRuntimeEnvironment Google.Chrome Adobe.Acrobat.Reader.64-bit 9NZVDKPMR9RD 9NCTDW2W1BH8 9MVZQVXJBQ9V 9N95Q1ZZPMH4 9N4D0MSMP0PT 9N4D0MSMP0PT --accept-package-agreements
+if "%1"=="Y" exit /B
 pause
 goto menu
 
 :office
+REM essa seção dá opções de aplicativos para escritório gratuitos ou por assinatura, ao selecionar ele faz download e instala os respectivos aplicativos.
 cls
 echo ===============================================================================================================
 echo #                                                                                                             #
@@ -180,10 +193,10 @@ pause
 goto menu
 
 :otimizar
+REM Essa opção seleciona o modo de energia alto desempenho, verifica a atualização de aplicativos compatíveis com winget, executa todas as instancias do dism, executa a verificação de arquivos do sistema, desfragmenta e invoca o checkdisk
 cls
 powercfg -setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 winget upgrade --all --include-unknown
-dism.exe /online /cleanup-image /scanhealth
 dism.exe /online /cleanup-image /restorehealth
 dism.exe /online /cleanup-image /startcomponentcleanup
 sfc /scannow
@@ -196,33 +209,174 @@ chkdsk /F /R
 pause
 goto menu
 
+:apps
+cls
+echo ===============================================================================================================
+echo #                                                                                                             #
+echo #                                          Selecione a opção desejada:                                        #
+echo #                                                                                                             #
+echo #                                          1 - Pesquisar Apps                                                 #
+echo #                                          2 - Atualizar Apps instalados                                      #
+echo #                                          0 - Voltar                                                         #
+echo #                                                                                                             #
+echo ===============================================================================================================
+set /p opcao=Opcao:
+if "%opcao%"=="1" goto pesquisar
+if "%opcao%"=="2" goto atualizar
+if "%opcao%"=="0" goto menu
+goto menu
+
 :pesquisar
+REM Dá uma interface para pesquisa e instalação de aplicativos através do winget
 cls
 set /p app=Digite o nome do aplicativo:
-winget search %app%
+winget search "%app%"
 
 echo O que deseja fazer?:
 echo 1 - Instalar
 echo 2 - Continuar pesquisando
 echo 0 - Voltar
-set /p opcao=Opcao:
-if "%opcao%"=="1" goto winget
+set /p opcao="Opcao:"
+if "%opcao%"=="1" goto instalarapp
 if "%opcao%"=="2" goto pesquisar
 if "%opcao%"=="0" goto menu
 
-:winget
-set /p app=Digite o ID do aplicativo:
-winget install %app%
+:instalarapp
+set /p app="Digite o ID do aplicativo:"
+winget install "%app%"
 pause
 goto menu
 
 :atualizar
+REM Atualiza os aplicativos do pc compatíveis com winget.
 cls
 winget upgrade --all --include-unknown
 pause
 goto menu
 
+:problemas
+REM Essa seção dá opções de solução para problemas comuns e conhecidos do windows.
+cls
+echo ===============================================================================================================
+echo #                                                                                                             #
+echo #                                                   Selecione:                                                #
+echo #                                                                                                             #
+echo #                                               1 - Limpar temps e prefetch (Reiniciar)                       #
+echo #                                               2 - Resetar Defender                                          #
+echo #                                               3 - Resetar IP                                                #
+echo #                                               0 - Voltar                                                    #
+echo #                                                                                                             #
+echo ===============================================================================================================
+set /p opcao=Opcao:
+if "%opcao%"=="1" goto temp
+if "%opcao%"=="2" goto defender
+if "%opcao%"=="3" goto resetip
+if "%opcao%"=="0" goto menu
+pause
+goto menu
+
+:temp
+cls
+echo Ao finalizar o procedimento, digite exit e reinicie o pc.
+pause
+rmdir /q /s "%systemroot%\Prefetch\"
+rmdir /q /s "%systemroot%\temp\"
+rmdir /q /s "%temp%\"
+pause
+goto problemas
+
+:defender
+cls
+start "ps" /wait powershell.exe -Command "Get-AppxPackage Microsoft.SecHealthUI -AllUsers | Reset-AppxPackage"
+pause
+goto problemas
+
+:resetip
+cls
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+pause
+goto problemas
+
+:drivers
+REM Essa seção dá opções de drivers para serem baixados e instalados no seu pc, alguns são automatizados, outros somente execução manual.
+cls
+echo ===============================================================================================================
+echo #                                                                                                             #
+echo #                                              Selecione a marca do dispositivo:                              #
+echo #                                                                                                             #
+echo #                                               1 - Lenovo                                                    #
+echo #                                               2 - Dell                                                      #
+echo #                                               3 - Acer (Beta)                                               #
+echo #                                               4 - HP                                                        #
+echo #                                               5 - Asus                                                      #
+echo #                                               6 - Asrock                                                    #
+echo #                                               0 - Voltar                                                    #
+echo #                                                                                                             #
+echo ===============================================================================================================
+set /p opcao=Opcao:
+if "%opcao%"=="1" goto lenovo
+if "%opcao%"=="2" goto dell
+if "%opcao%"=="3" goto acer
+if "%opcao%"=="4" goto hp
+if "%opcao%"=="5" goto asus
+if "%opcao%"=="6" goto asrock
+if "%opcao%"=="0" goto menu
+goto menu
+
+:lenovo
+cls
+winget install Lenovo.SystemUpdate
+echo Execute o aplicativo "System Update" e avance para a instalação de drivers do seu lenovo.
+pause
+goto menu
+
+:dell
+cls
+start "web" "https://dl.dell.com/serviceability/eSupport/SupportAssistLauncher.exe"
+echo Execute o aplicativo "SupportAssistLauncher.exe" que foi baixado na pasta downloads, ao finalizar a instalação 
+echo clique na seta inferior na janela aberta e utilize a seção drivers para buscar e instalar os mesmos.
+pause
+goto menu
+
+:acer
+cls
+Winget install ULICTekInc.AcerSense5.0_nt9dgb7efx6bt
+echo Execute o aplicativo "AcerSense", siga as instruções, ao chegar na pagina principal utilize a seção drivers
+echo para buscar e instalar os mesmos.
+pause
+goto menu
+
+:hp
+cls
+start "web" "https://support.hp.com/br-pt/drivers"
+echo Siga as instruções do site para selecionar seu dispositivo, selecione "Deixe a HP detectar seu produto" e 
+echo inicie o download, execute o app, instale, vá em meu dispositivo e na aba downloads utilize a seção drivers
+echo para buscar e instalar os mesmos.
+pause
+goto menu
+
+:asus
+cls
+start "web" "https://www.asus.com/br/support/download-center/"
+echo Siga as instruções do site para localizar o modelo, selecione suporte, drivers e firmware, selecione a versão
+echo do sistema operacional, baixe os drivers desejados e execute manualmente um por um usando "Setup" dentro das
+echo pastas baixadas no site.
+pause
+goto menu
+
+:asrock
+cls
+start "web" "https://www.asrock.com/support/index.br.asp"
+echo Siga as instruções do site para localizar o modelo, selecione suporte, drivers e firmware, selecione a versão
+echo do sistema operacional, baixe os drivers desejados e execute manualmente um por um usando "Setup" dentro das
+echo pastas baixadas no site.
+pause
+goto menu
+
 :sobre
+REM Essa seção serve apenas para créditos, texto explicativo e direcionamento para sites oficiais.
 cls
 echo ===============================================================================================================
 echo #                                                                                                             #
@@ -237,7 +391,6 @@ set /p opcao=Opcao:
 if "%opcao%"=="1" goto manual
 if "%opcao%"=="2" goto creditos
 if "%opcao%"=="0" goto menu
-pause
 goto menu
 
 :manual
@@ -257,10 +410,9 @@ echo #   Opção 3 Instalar programas básicos: Instala os mesmos apps da Opçã
 echo #   Opção 4 Instalar Offices: Mostra opções de aplicativos para escritório para instalação.                   #
 echo #   Opção 5 Otimizar: Coloca no alto desempenho, atualiza todos os apps compatíveis com winget, executa       #
 echo # sfc /scannow, dism, desfragmenta todos os discos conectados e executa o chkdsk (Opcional).                  #
-echo #   Opção 6 Pesquisar App: Faz uma pesquisa com o termo digitado nos repositorios winget e pergunta qual      #
-echo # gostaria de instalar, copie a id do app selecionando com o mouse e apertando enter para copiar e cole a id  #
-echo # com Ctrl + V para instalar o app desejado.                                                                  #
-echo #   Opção 7 atualiza todos os apps compatíveis com winget.                                                    #
+echo #   Opção 6 Aplicativos: Dá opções para pesquisar apps compatíveis com winget ou atualizar eles.              #
+echo #   Opção 7 Resolução de problemas: Dá opções que podem solucionar problemas do windows.                      #
+echo #   Opção 8 Drivers: Dá opções de fabricantes para buscar drivers de dispositivos.                            #
 echo #                                                                                                             #
 echo ===============================================================================================================
 pause
